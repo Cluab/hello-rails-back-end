276d498d1d0ff6fd3e751757c9b27e9c9418b058
cluab <cluab11222@gmail.com>
2023-03-28 11:05:32 +0200

    add database

---
+++ db/migrate/20230328090245_create_create_messages.rb
@@ -0,0 +1,9 @@
+class CreateCreateMessages < ActiveRecord::Migration[7.0]
+  def change
+    create_table :create_messages do |t|
+      t.string :greeting
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CreateMessage.create(greeting: "Hello, world!")
+CreateMessage.create(greeting: "Hi there!")
+CreateMessage.create(greeting: "Welcome!")
+CreateMessage.create(greeting: "Hey, how are you?")
+CreateMessage.create(greeting: "Greetings!")
